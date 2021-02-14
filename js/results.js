af8bb7f0393fb02c177e95ee93d7e3a64306d588
CLoudlessdreams <aji.khan88@gmail.com>
2021-02-14 14:45:39 +0000

    first draft

--- js/results.js
+++ js/results.js
@@ -1,8 +1,10 @@
-
+// saves the data to localStorage
 function handleSubmit () {
   const datetime = document.getElementById('datetimepick').value;
   const from = document.getElementById('pickup').value;
   const to = document.getElementById('destination').value;
+
+  const passenger = document.getElementById('people').value;
 
   const rtnDatetime = document.getElementById('datetimepick2').value;
   const rtnFrom = document.getElementById('rtn-pickup').value;
@@ -11,13 +13,13 @@ function handleSubmit () {
   localStorage.setItem("datetimepick", datetime)
   localStorage.setItem("pickup", from)
   localStorage.setItem("destination", to)
+  localStorage.setItem("people", passenger)
 
   localStorage.setItem("datetimepick2", rtnDatetime)
   localStorage.setItem("rtn-pickup", rtnFrom)
   localStorage.setItem("rtn-destination", rtnFrom)
 
+
   return;
-
-
 
 }
